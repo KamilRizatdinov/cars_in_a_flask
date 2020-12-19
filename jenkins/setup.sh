a22d8d8e052b604b282b5ba94da506cab9b77e12
TAPAKAH20 <tapakah20@ya.ru>
2020-12-19 22:08:47 +0300

    Added bash scripts for jenkins

---
+++ jenkins/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+virtualenv --python=python3 env --no-site-packages
+source env/bin/activate
+pip install -r requirements.txt
+
+export FLASK_APP=app.py
+flask db migrate
+flask db upgrade
