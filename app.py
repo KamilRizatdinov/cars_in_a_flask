f8711516fa7542400bbd464ddb9403f958a3bf0b
Kamil Rizatdinov <k.rizatdinov@innopolis.university>
2020-12-20 17:51:50 +0300

    Change postgres connection URL

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/cars_api"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://root:root@192.168.195.154:5432/cars_api"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
