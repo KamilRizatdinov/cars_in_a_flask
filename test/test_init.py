60c163d5c6d2bc42a16ade3750d78923189f5b17
rufusnufus <nufusrufus@gmail.com>
2020-12-21 19:28:43 +0300

    add tests checking functionality

--- test/test_init.py
+++ test/test_init.py
@@ -17,3 +17,32 @@ def test_base_check_content_type_equals_json():
 def test_cars_check_content_type_equals_json():
     response = requests.get(f'{BASE_URL}cars')
     assert response.headers["Content-Type"] == "application/json"
+
+def test_car_add_functionality():
+	response = requests.post(f'{BASE_URL}cars', json={"name": "toyota", "model": "x4", "doors": 5}, headers={"Content-Type":"application/json"})
+	assert response.status_code == 200
+
+def test_car_presence_functionality():
+	response = requests.get(f'{BASE_URL}cars')
+	present = False
+	if response.status_code == 200:
+		# print(response.content["cars"]["name"])
+		for car in response.json()["cars"]:
+			if car["name"] == "toyota" and car["model"] == "x4" and car["doors"] == 5:
+				present = True
+	assert present
+
+def test_car_delete_functionality():
+	response = requests.get(f'{BASE_URL}cars')
+	deleted = False
+	if response.status_code == 200:
+		for car in response.json()["cars"]:
+			if car["name"] == "toyota" and car["model"] == "x4" and car["doors"] == 5:
+				id = car["id"]
+				response = requests.delete(f'{BASE_URL}cars/{id}')
+				if response.status_code != 200:
+					assert False
+				else:
+					resp = requests.get(f'{BASE_URL}cars/{id}')
+					assert resp.status_code == 404
+	
